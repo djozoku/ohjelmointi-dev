7dd124fbe29a49ace174340283b4bd4e73487453
Ozoku <dragon.djozoku@gmail.com>
2019-05-23 11:13:52 +0300

    remove webpack config because it is no longer needed

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -2,12 +2,4 @@ const path = require('path');
 const withTypescript = require('@zeit/next-typescript');
 module.exports = withTypescript({
   distDir: '../../dist/functions/next',
-  // TODO: remove this as shared won't have credentials stored anymore as firebase-admin sdk is soon being served through the express server
-  webpack(config, options) {
-    config.module.rules.push({
-      test: path.resolve(__dirname, '..', 'shared', 'credentials', 'server.ts'),
-      use: 'null-loader',
-    });
-    return config;
-  },
 });
